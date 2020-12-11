f5b6d8b3bfa4b2f68e3eb4bdd05086f102a6256a
ufuk-tech-name <ufuk-tech-name@hotmail.com>
2020-12-11 18:46:36 +0200

    step2

--- src/index.js
+++ src/index.js
@@ -15,18 +15,22 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [allClicks, setAll] = useState(0)
 
 
   return (
     <div>
       <h1>give feedback</h1>
-      <Button handleClick={() => setGood(good + 1)} text="good" />
-      <Button handleClick={() => setNeutral(neutral + 1)} text="neutral" />
-      <Button handleClick={() => setBad(bad + 1)} text="bad" />
+      <Button handleClick={() => { setGood(good + 1); setAll(allClicks + 1) }} text="good" />
+      <Button handleClick={() => { setNeutral(neutral + 1); setAll(allClicks + 1) }} text="neutral" />
+      <Button handleClick={() => { setBad(bad + 1); setAll(allClicks + 1) }} text="bad" />
       <h1>statistics</h1>
       <Display text="good" value={good} />
       <Display text="neutral" value={neutral} />
       <Display text="bad" value={bad} />
+      <Display text="all" value={allClicks} />
+      <Display text="average" value={(good * 1 + neutral * 0 - bad * 1) / allClicks} />
+      <Display text="positive" value={(100 * good / allClicks) + "%"} />
     </div>
   )
 }
