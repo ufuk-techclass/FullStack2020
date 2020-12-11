e309ca4b48faf1768b7c3bec94b7f5385e98f1dc
ufuk-tech-name <ufuk-tech-name@hotmail.com>
2020-12-11 19:31:39 +0200

    step3

--- src/index.js
+++ src/index.js
@@ -2,29 +2,37 @@ import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
 const App = (props) => {
+
     const [selected, setSelected] = useState(0)
-    const [vote, setVote] = useState(
-        { 0: 0, 1: 0, 2: 0, 3: 0, 4: 0, 5: 0 }
-    )
+    const [vote, setVote] = useState({ 0: 0, 1: 0, 2: 0, 3: 0, 4: 0, 5: 0 })
+    const [max, setMax] = useState({ key: 0, value: 0 })
 
     const handleVote = (selected) => {
-        const newVote = { ...vote }
-        newVote[selected] += 1;
-        //setVote({...vote, selected: newVote })
-        setVote(newVote)
 
+        const newVote = { ...vote };
+        newVote[selected] += 1;
+        setVote(newVote);
+
+        //find max value, then find the key of the max value
+        const maxValue = Math.max(...Object.values(newVote));
+        const keyOfMax = Object.keys(newVote).find(key => newVote[key] === maxValue);
+        setMax({ key: keyOfMax, value: maxValue });
     }
 
     const handleNext = () => {
-        setSelected(Math.floor(Math.random() * 6))
+        setSelected(Math.floor(Math.random() * 6));
     }
 
     return (
         <div>
+            <h1>Anecdote of the day</h1>
             <p>{props.anecdotes[selected]}</p>
             <p>has {vote[selected]} votes</p>
             <button onClick={() => handleVote(selected)} >vote</button>
             <button onClick={handleNext} >next anecdote</button>
+            <h1>Anecdote with most vote</h1>
+            <p>{props.anecdotes[max.key]}</p>
+            <p>has {max.value} votes</p>
         </div>
     )
 }
