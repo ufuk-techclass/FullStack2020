f5e63e054697098afdcdd460e3a5cf192199eeb5
ufuk-tech-name <ufuk-tech-name@hotmail.com>
2020-12-12 20:30:33 +0200

    step6

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,26 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import Persons from './Persons'
 import Filter from './Filter'
 import PersonForm from './PersonForm'
+import axios from 'axios'
 
 const App = () => {
-    const [persons, setPersons] = useState([
-        { name: 'Arto Hellas', number: '040-1234567', id: 0 },
-        { name: 'Ada Lovelace', number: '39-44-5323523', id: 1 },
-        { name: 'Dan Abramov', number: '12-43-234345', id: 2 },
-        { name: 'Mary Poppendieck', number: '39-23-6423122', id: 3 }
-    ])
+    const [persons, setPersons] = useState([])
     const [newName, setNewName] = useState('')
     const [newNumber, setNewNumber] = useState('')
     const [myFilter, setMyFilter] = useState('')
+
+    const hook = () => {
+        console.log('effect')
+        axios
+            .get('http://localhost:3001/persons')
+            .then(response => {
+                console.log('promise fulfilled')
+                setPersons(response.data)
+            })
+    }
+
+    useEffect(hook, [])
 
     //create Id
     let randomId = () => {
