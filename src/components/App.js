5a3f3c6959f5688609778b713f52a27af64e204e
ufuk-tech-name <ufuk-tech-name@hotmail.com>
2020-12-12 20:22:54 +0200

    step1

---
+++ src/components/App.js
@@ -0,0 +1,52 @@
+import React, { useState } from 'react'
+
+const App = () => {
+    const [persons, setPersons] = useState([
+        { name: 'Arto Hellas', id: 0 }
+    ])
+    const [newName, setNewName] = useState('')
+
+    //create Id
+    let randomId = () => {
+        return Math.random();
+    };
+
+    const addNote = (event) => {
+        event.preventDefault()
+        console.log('button clicked', event.target)
+
+        const noteObject = {
+            name: newName, id: randomId()
+        }
+
+        setPersons(persons.concat(noteObject))
+        setNewName('')
+    }
+
+    const handleNoteChange = (event) => {
+        console.log(event.target.value)
+        setNewName(event.target.value)
+    }
+
+    return (
+        <div>
+            <h2>Phonebook</h2>
+            <form onSubmit={addNote}>
+                <div>
+                    name: <input value={newName} onChange={handleNoteChange} />
+                </div>
+                <div>
+                    <button type="submit">add</button>
+                </div>
+            </form>
+            <h2>Numbers</h2>
+            <div>
+                {persons.map(e =>
+                    <div key={e.id} > {e.name} </div>
+                )}
+            </div>
+        </div>
+    )
+}
+
+export default App
