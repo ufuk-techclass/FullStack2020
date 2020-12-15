2217e04bf4a71a73103073bc31f835224bd39740
ufuk-tech-name <ufuk-tech-name@hotmail.com>
2020-12-16 01:58:05 +0200

    command-line_database

---
+++ index.js
@@ -0,0 +1,102 @@
+const express = require('express')
+const morgan = require('morgan')
+const app = express()
+const cors = require('cors')
+
+app.use(cors())
+app.use(express.static('build'))
+
+morgan.token('json', (request, response) => JSON.stringify(request.body))
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :json'))
+
+let persons = [
+    { name: 'Arto Hellas', number: '040-1234567', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+]
+
+app.use(express.json())
+
+const unknownEndpoint = (request, response) => {
+    response.status(404).send({ error: 'unknown endpoint' })
+}
+
+
+
+//create Id 
+let randomId = () => {
+    return Date.now();
+};
+
+app.get('/api/persons', (request, response) => {
+
+    response.json(persons)
+})
+
+app.get('/api/persons/:id', (request, response) => {
+
+    const id = Number(request.params.id)
+    const person = persons.find(person => person.id === id)
+
+    if (person) {
+        response.json(person)
+    } else {
+        response.status(404).end()
+    }
+})
+
+app.get('/info', (request, response) => {
+
+    response.write(`<p>Phonebook has info for ${persons.length} people</p>`);
+    response.write(`<p> ${Date()} !</p>`);
+    response.end();
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    persons = persons.filter(person => person.id !== id)
+
+    response.status(204).end()
+})
+
+app.post('/api/persons', (request, response) => {
+    const body = request.body
+
+    /*  if (!body.name || !body.number || persons.filter(person => person.name === body.name).length !== 0) {
+  
+          return response.status(400).json({
+              error: 'content missing'
+          })
+      }
+      */
+
+    if (body.name.length == 0 || body.number.length == 0) {
+
+        return response.status(400).json({
+            error: 'name or number is missing'
+        })
+    }
+
+    if (persons.filter(person => person.name === body.name).length !== 0) {
+        return response.status(409).json({
+            error: `name must be unique. conflict: ${body.name}`
+        })
+    }
+
+    const person = {
+        name: body.name,
+        number: body.number,
+        id: randomId()
+    }
+
+    persons = persons.concat(person)
+    response.json(person)
+})
+
+app.use(unknownEndpoint)
+
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
---
+++ mongo.js
@@ -0,0 +1,55 @@
+const mongoose = require('mongoose')
+
+const password = process.argv[2]
+
+const url =
+    `mongodb+srv://fs2020:${password}@cluster0.lhwqp.mongodb.net/new_app?retryWrites=true&w=majority`
+//  `mongodb+srv://fullstack:${password}@cluster0-ostce.mongodb.net/test?retryWrites=true`
+
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
+
+//Schema
+const personSchema = new mongoose.Schema({
+    name: String,
+    number: String,
+    id: Number,
+})
+
+//Model
+const Person = mongoose.model('Person', personSchema)
+
+if (process.argv.length < 3) {
+    console.log('Please provide the password as an argument: node mongo.js <password>')
+    process.exit(1)
+}
+
+else if (process.argv.length == 3) {
+    Person.find({}).then(result => {
+        console.log("phonebook: ");
+        result.forEach(person => {
+            console.log(`${person.name} ${person.number}`)
+        })
+        mongoose.connection.close()
+    })
+}
+
+else if (process.argv.length == 5) {
+    const person = new Person({
+        name: process.argv[3],
+        number: process.argv[4],
+        id: Date.now()
+    })
+
+    person.save().then(result => {
+        console.log(`added ${process.argv[3]} number ${process.argv[4]} to phonebook`)
+        mongoose.connection.close()
+    })
+}
+
+else {
+    console.log('Please provide name and number as an argument: node mongo.js <password> <name> <number>')
+    mongoose.connection.close()
+}
+
+
+
