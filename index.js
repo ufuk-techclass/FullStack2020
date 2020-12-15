e34df68821672fa266b921a8b6af1dd76df78ab0
ufuk-tech-name <ufuk-tech-name@hotmail.com>
2020-12-16 01:11:06 +0200

    step4

--- index.js
+++ index.js
@@ -39,6 +39,13 @@ app.get('/info', (request, response) => {
     response.end();
 })
 
+app.delete('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    persons = persons.filter(person => person.id !== id)
+
+    response.status(204).end()
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
