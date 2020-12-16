75152b605ff1985f588b64ba1dbbe5f273008a6a
ufuk-tech-name <ufuk-tech-name@hotmail.com>
2020-12-16 03:41:29 +0200

    lint_config

---
+++ models/person.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose')
+var uniqueValidator = require('mongoose-unique-validator')
+
+const url = process.env.MONGODB_URI
+
+console.log('connecting to', url)
+
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
+  .then(() => {
+    console.log('connected to MongoDB')
+  })
+  .catch((error) => {
+    console.log('error connecting to MongoDB:', error.message)
+  })
+
+//Schema
+const personSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    unique: true,
+    minlength: 3
+  },
+  number: {
+    type: String,
+    minlength: 8
+  },
+  id: String
+})
+
+personSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
+})
+
+personSchema.plugin(uniqueValidator)
+
+module.exports = mongoose.model('Person', personSchema)
