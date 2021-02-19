975857b261b09e0405327d9ad815dda36c50572d
ufuk-tech-name <ufuk-tech-name@hotmail.com>
2021-02-19 16:27:07 +0200

    step11

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
 const LoginForm = ({ handleLogin, handleUsernameChange,
   handlePasswordChange, username, password }) => {
@@ -26,6 +27,14 @@ const LoginForm = ({ handleLogin, handleUsernameChange,
       </form>
     </div>
   )
+}
+
+LoginForm.propTypes = {
+  handleLogin: PropTypes.func.isRequired,
+  handleUsernameChange: PropTypes.func.isRequired,
+  handlePasswordChange: PropTypes.func.isRequired,
+  username: PropTypes.string.isRequired,
+  password: PropTypes.string.isRequired
 }
 
 export default LoginForm
