47d0c49b03b0c648100b53315b603c77972a96a1
ufuk-tech-name <ufuk-tech-name@hotmail.com>
2021-02-19 16:04:32 +0200

    step1

---
+++ tests/blog_api.test.js
@@ -0,0 +1,81 @@
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const app = require('../app')
+
+const api = supertest(app)
+
+// Initializing the database before tests
+
+const Blog = require('../models/blog')
+const initialBlogs = [
+  {
+    title: 'test-title-1',
+    author: 'test-author-1',
+    url: 'test-url-1',
+    likes: 1,
+    id: 13,
+  },
+  {
+    title: 'test-title-2',
+    author: 'test-author-2',
+    url: 'test-url-2',
+    likes: 2,
+    id: 14
+  },
+]
+
+beforeEach(async () => {
+  await Blog.deleteMany({})
+  let blogObject = new Blog(initialBlogs[0])
+  await blogObject.save()
+  blogObject = new Blog(initialBlogs[1])
+  await blogObject.save()
+})
+
+
+
+test('notes are returned as json', async () => {
+  await api
+    .get('/api/blogs')
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+})
+
+test('4.8 amount of blog posts', async () => {
+  const response = await api.get('/api/blogs')
+
+  expect(response.body).toHaveLength(2)
+})
+
+/*
+test('4.9 verifies id', async () => {
+  const response = await api.get('/api/blogs')
+
+  expect(response.body[0]._id).toBeDefined()
+})
+*/
+/*
+test('the first blog is about HTTP methods', async () => {
+  const response = await api.get('/api/blogs')
+
+  expect(response.body[0].likes).toBe(1)
+})
+
+test('all notes are returned', async () => {
+  const response = await api.get('/api/blogs')
+
+  expect(response.body).toHaveLength(initialBlogs.length)
+})
+
+test('a specific note is within the returned notes', async () => {
+  const response = await api.get('/api/blogs')
+
+  const contents = response.body.map(r => r.content)
+  expect(contents).toContain(
+    'Browser can execute only Javascript'
+  )
+})
+*/
+afterAll(() => {
+  mongoose.connection.close()
+})
