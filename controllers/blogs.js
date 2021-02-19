b629873e8ab0d38d3149c13f83fabd3346dbbc25
ufuk-tech-name <ufuk-tech-name@hotmail.com>
2021-02-19 16:14:25 +0200

    step5

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -12,7 +12,7 @@ blogsRouter.get('/', async (request, response) => {
     response.json(blogs)
   })
   */
-  const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 })
+  const blogs = await Blog.find({}).populate('user', { username: 1, name: 1, id: 1 })
   response.json(blogs)
 })
 
