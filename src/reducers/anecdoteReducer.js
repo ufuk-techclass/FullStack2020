2ce366eca5ecb3714298413f854e61efc829e907
ufuk-tech-name <ufuk-tech-name@hotmail.com>
2021-03-31 03:08:23 +0300

    step1

---
+++ src/reducers/anecdoteReducer.js
@@ -0,0 +1,44 @@
+const anecdotesAtStart = [
+  'If it hurts, do it more often',
+  'Adding manpower to a late software project makes it later!',
+  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+  'Premature optimization is the root of all evil.',
+  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
+]
+
+const getId = () => (100000 * Math.random()).toFixed(0)
+
+const asObject = (anecdote) => {
+  return {
+    content: anecdote,
+    id: getId(),
+    votes: 0
+  }
+}
+
+export const voteAnectode = (id) => {
+  return {
+    type: "VOTE",
+    id: id
+  }
+}
+
+const initialState = anecdotesAtStart.map(asObject)
+
+const reducer = (state = initialState, action) => {
+  console.log('state now: ', state)
+  console.log('action', action)
+
+  if (action.type === "VOTE") {
+    const anectode = state.find(anectode => anectode.id === action.id)
+    const changedState = { ...anectode, votes: anectode.votes + 1 }
+
+    return state.map(anectode =>
+      anectode.id !== action.id ? anectode : changedState)
+  }
+
+  return state
+}
+
+export default reducer
